46cf2f50f16ce84c1c32ea69e075057c8d92d488
Roni Choudhury <roni.choudhury@kitware.com>
2019-03-05 17:03:32 -0500

    Add a real endpoint to retrieve vertex data

--- multinet/multinet/__init__.py
+++ multinet/multinet/__init__.py
@@ -1,22 +1,34 @@
 from girder import plugin
 from girder.api import access
-from girder.api.rest import Resource
+from girder.api.rest import Resource, RestException
 from girder.api.describe import Description, autoDescribeRoute
+
+import requests
 
 
 class MultiNet(Resource):
-    def __init__(self):
+    def __init__(self, port):
         super(MultiNet, self).__init__()
         self.resourceName = 'multinet'
+        self.arango_port = port
 
-        self.route('GET', ('greeting',), self.get_greeting)
+        self.route('GET', ('vertices',), self.get_vertices)
 
     @access.public
     @autoDescribeRoute(
-        Description('Get a greeting')
+        Description('Foobar')
+        .param('db', 'The database', dataType='string', required=True)
+        .param('collection', 'The collection', dataType='string', required=True)
     )
-    def get_greeting(self):
-        return '¡Hola!'
+    def get_vertices(self, params):
+        db = params['db']
+        collection = params['collection']
+
+        r = requests.get('http://localhost:8080/vertices/{}/graph/{}'.format(db, collection))
+        if r.status_code == requests.codes.ok:
+            return r.json()
+
+        raise RestException(r.reason, r.status_code)
 
 
 class GirderPlugin(plugin.GirderPlugin):
@@ -24,4 +36,4 @@ class GirderPlugin(plugin.GirderPlugin):
 
     def load(self, info):
         # add plugin loading logic here
-        info['apiRoot'].multinet = MultiNet()
+        info['apiRoot'].multinet = MultiNet(port=8080)

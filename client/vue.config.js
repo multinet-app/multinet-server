9d4d8208748aea9bbfa9c215613f1a76ad6d55a7
Jake Nesbitt <jjnesbitt2@gmail.com>
2019-08-14 16:40:50 -0400

    Update vue dotenv path for server directory move

--- client/vue.config.js
+++ client/vue.config.js
@@ -6,7 +6,7 @@ const dotenv = require('dotenv');
 const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin');
 
 // Read in .env file.
-const env = dotenv.parse(fs.readFileSync(path.resolve('..', 'server', '.env')));
+const env = dotenv.parse(fs.readFileSync(path.resolve('..', '.env')));
 process.env.FLASK_SERVE_PORT = env.FLASK_SERVE_PORT || 5000;
 
 module.exports = {

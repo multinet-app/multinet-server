54805e8c6ee3d31bab5da644a93a39ed765d32fb
Jake Nesbitt <jjnesbitt2@gmail.com>
2019-08-20 15:54:22 -0400

    Remove tox.ini and run black

--- test/test_uploaders/test_csv.py
+++ test/test_uploaders/test_csv.py
@@ -1,1 +1,18 @@
 """Tests functions in the CSV Uploader Flask Blueprint."""
+import csv
+
+from multinet.uploaders.csv import validate_csv
+
+DUPLICATE_KEYS = """
+_key, name
+1, a
+2, b
+1, c
+"""
+
+
+def test_validate_csv():
+    """Tests the validate_csv function."""
+    rows = list(csv.DictReader(DUPLICATE_KEYS))
+    print(rows)
+    validate_csv(rows)

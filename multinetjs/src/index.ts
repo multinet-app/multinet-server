165b9d72a80480df13d182d607ccce8d8966af42
Roni Choudhury <roni.choudhury@kitware.com>
2019-10-08 10:04:29 -0400

    Swap out webpack for plain old tsc

--- multinetjs/src/index.ts
+++ multinetjs/src/index.ts
@@ -1,3 +1,18 @@
 export function add(x: number, y: number): number {
-  return x + y;
+  return x + y + 3.1;
+}
+
+export function sub(x: number, y: number) {
+  return x - y;
+}
+
+export const pi = 3.14;
+
+function timeout(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+export async function waitandsay(msg: string, delay: number) {
+  await timeout(delay);
+  console.log(msg);
 }

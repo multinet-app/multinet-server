2f516ede47b7623af55e2803ef377354c21f62f6
Roni Choudhury <roni.choudhury@kitware.com>
2019-10-14 18:13:02 -0400

    Add GET interface

--- multinetjs/src/index.ts
+++ multinetjs/src/index.ts
@@ -1,18 +1,85 @@
-export function add(x: number, y: number): number {
-  return x + y + 3.1;
+import axios, { AxiosInstance } from 'axios';
+
+class Client {
+  axios: AxiosInstance;
+
+  constructor(baseURL: string) {
+    this.axios = axios.create({
+      baseURL,
+    });
+  }
+
+  get(path: string): Promise<string[]> {
+    return new Promise((resolve, reject) => {
+      this.axios.get(path)
+        .then(resp => {
+          resolve(resp.data);
+        })
+        .catch(resp => {
+          reject(resp.response);
+        });
+    });
+  }
 }
 
-export function sub(x: number, y: number) {
-  return x - y;
+class MultinetAPI {
+  client: Client;
+
+  constructor(baseURL: string) {
+    this.client = new Client(baseURL);
+  }
+
+  workspaces(): Promise<string[]> {
+    return this.client.get('workspaces');
+  }
+
+  workspace(workspace: string): Promise<any> {
+    if (!workspace) {
+      throw new Error('argument "workspace" must not be empty');
+    }
+
+    return this.client.get(`workspaces/${workspace}`);
+  }
+
+  tables(workspace: string): Promise<any> {
+    return this.client.get(`workspaces/${workspace}/tables`);
+  }
+
+  table(workspace: string, table: string, offset: int = 0, limit: int = 30): Promise<any> {
+    return this.client.get(`workspaces/${workspace}/tables/${table}`, {
+      offset,
+      limit,
+    });
+  }
+
+  graphs(workspace: string): Promise<any> {
+    return this.client.get(`workspaces/${workspace}/graphs`);
+  }
+
+  graph(workspace: string, graph: string): Promise<any> {
+    return this.client.get(`workspaces/${workspace}/graphs/${graph}`);
+  }
+
+  nodes(workspace: string, graph: string, offset: int = 0, limit: int = 30): Promise<any> {
+    return this.client.get(`workspaces/${workspace}/graphs/${graph}/nodes`, {
+      offset,
+      limit,
+    });
+  }
+
+  attributes(workspace: string, graph: string, nodeId: string): Promise<any> {
+    return this.client.get(`workspaces/${workspace}/graphs/${graph}/${nodeId}/attributes`);
+  }
+
+  edges(workspace: string, graph: string, nodeId: string, direction: string = 'all', offset: int = 0, limit: int = 30) {
+    return this.client.get(`workspaces/${workspace}/graphs/${graph}/${nodeId}/edges`, {
+      direction,
+      offset,
+      limit,
+    });
+  }
 }
 
-export const pi = 3.14;
-
-function timeout(ms: number) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-export async function waitandsay(msg: string, delay: number) {
-  await timeout(delay);
-  console.log(msg);
+export function multinetApi(baseURL: string) {
+  return new MultinetAPI(baseURL);
 }

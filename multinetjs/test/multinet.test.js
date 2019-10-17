6657912675d42cb9735e07c8d4629392dd6eb3f2
Roni Choudhury <roni.choudhury@kitware.com>
2019-10-17 10:58:13 -0400

    Add infrastructure for server integration tests

--- multinetjs/test/multinet.test.js
+++ multinetjs/test/multinet.test.js
@@ -1,15 +1,21 @@
 import test from 'tape';
 import process from 'process';
 
-console.log(process.cwd());
-
-
 import { multinetApi } from '../dist';
 
-
-test('multinet test', (t) => {
-  t.plan(2);
-
+test('multinet test', async (t) => {
   t.ok(multinetApi, 'multinetApi() was imported successfully');
   t.equal(typeof multinetApi, 'function', 'multinetApi() is a function');
+
+  const api = multinetApi('http://localhost:50000/api');
+  t.ok(api, 'multinetApi() gave us an API object');
+
+  try {
+    const workspaces = await api.workspaces();
+    t.deepEqual(workspaces, [], 'new server should have no workspaces');
+  } catch(e) {
+    t.fail(`api.workspaces() failed: ${e.status} ${e.statusText}`);
+  }
+
+  t.end();
 });
--- scripts/server/start.sh
+++ scripts/server/start.sh
@@ -7,8 +7,36 @@ if [ -e server.pid ]; then
 fi
 
 # Launch the server in the background.
-nohup pipenv run serve &
+PIPENV_DONT_LOAD_ENV=1 FLASK_SERVE_PORT=50000 ARANGO_PORT=58529 nohup pipenv run serve >server.out &
 echo $! >server.pid
 
 # Start the Arango database in the background with a clean data directory.
-ARANGO_DATA=$(readlink -f arango) ARANGO_USER=$(id -u) ARANGO_GROUP=$(id -g) docker-compose up -d
+ARANGO_PORT=58529 ARANGO_DATA=$(readlink -f arango) docker-compose up -d
+
+# Loop until the server is up.
+started=0
+count=0
+
+echo -n "waiting for server to come up"
+while [ ${started} = 0 ] && [ ${count} -lt 30 ]; do
+    headers=$(curl -s -I http://localhost:50000/api/workspaces)
+    curl_status=$?
+
+    if [ ${curl_status} = 0 ]; then
+        result=$(echo ${headers} | head -1 | grep 200)
+        good=$?
+        if [ ${good} = 0 ]; then
+            started=1
+        fi
+    fi
+
+    sleep 1
+    count=$(expr ${count} + 1)
+    echo -n .
+done
+
+if [ ${started} == 1 ]; then
+    true
+else
+    false
+fi

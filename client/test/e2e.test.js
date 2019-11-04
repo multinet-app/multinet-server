65bafe1fe3d8a760c42d74d1ced655f67c92fb51
Roni Choudhury <roni.choudhury@kitware.com>
2019-11-04 16:52:22 -0500

    Demonstrate puppeteer working with tape

--- client/test/e2e.test.js
+++ client/test/e2e.test.js
@@ -1,28 +1,25 @@
 import test from 'tape';
 import puppeteer from 'puppeteer';
 
-let browser;
 const width = 1920;
 const height = 1080;
 
-// const browser = puppeteer.launch({
-//     headless: false,
-//     args: [`--window-size=${width},${height}`],
-//     slowMo: 200
-// })
+function browser(width, height) {
+  return puppeteer.launch({
+    headless: true,
+    args: [`--window-size=${width},${height}`],
+    slowMo: 200
+  });
+}
 
-test('e2e client test', async (t) => { 
-    t.ok("hello", "It's working.");
+test('e2e client test', async (t) => {
+  t.ok("hello", "It's working.");
 
-    const browser = await puppeteer.launch({
-        headless: false,
-        args: [`--window-size=${width},${height}`],
-        slowMo: 200
-    })
+  const b = await browser(width, height);
 
-    browser
-    .then(d => {t.ok(d, "puppeteer is live"); return d;})
-    .then(d => d.close())
+  t.ok(browser, "puppeteer is live");
 
-    t.end();
-})
+  await b.close();
+
+  t.end();
+});

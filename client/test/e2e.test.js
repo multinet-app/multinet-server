bb875b70e5657931080ebf2f4f901d9f50811c76
JackWilb <jackwilburn@tutanota.com>
2019-11-04 14:38:31 -0700

    update package json

--- client/test/e2e.test.js
+++ client/test/e2e.test.js
@@ -5,17 +5,24 @@ let browser;
 const width = 1920;
 const height = 1080;
 
-browser = async () => await puppeteer.launch({
-    headless: false,
-    args: [`--window-size=${width},${height}`],
-    slowMo: 200
-})
+// const browser = puppeteer.launch({
+//     headless: false,
+//     args: [`--window-size=${width},${height}`],
+//     slowMo: 200
+// })
 
 test('e2e client test', async (t) => { 
     t.ok("hello", "It's working.");
 
-    browser()
-    .then(d => {t.ok(d, "puppeteer is live");})
+    const browser = await puppeteer.launch({
+        headless: false,
+        args: [`--window-size=${width},${height}`],
+        slowMo: 200
+    })
+
+    browser
+    .then(d => {t.ok(d, "puppeteer is live"); return d;})
+    .then(d => d.close())
 
     t.end();
 })

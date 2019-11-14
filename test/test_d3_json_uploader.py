fdc02f1bb10b398ba56a358b9b2f818219d7308a
JackWilb <jackwilburn@tutanota.com>
2019-11-14 13:31:36 -0700

    format all the tests in the right way

--- test/test_d3_json_uploader.py
+++ test/test_d3_json_uploader.py
@@ -11,20 +11,14 @@ TEST_DATA_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "data"))
 
 def test_validate_d3_json():
     """Tests the validate_csv function."""
-    # Arrange: Set file paths
-    # good_file = os.path.join(TEST_DATA_DIR, "miserables.json")
-    duplicated_nodes = os.path.join(TEST_DATA_DIR, "miserables_duplicate_nodes.json")
-    incon_keys = os.path.join(TEST_DATA_DIR, "miserables_inconsistent_link_keys.json")
-    inval_keys = os.path.join(TEST_DATA_DIR, "miserables_invalid_link_keys.json")
-
     # Arrange: Import the les miserables datasets with and without errors
     with open(test_data_path("miserables.json")) as f:
         good_data = json.load(f, object_pairs_hook=OrderedDict)
-    with open(duplicated_nodes) as f:
+    with open(test_data_path("miserables_duplicate_nodes.json")) as f:
         dup_node_data = json.load(f, object_pairs_hook=OrderedDict)
-    with open(incon_keys) as f:
+    with open(test_data_path("miserables_inconsistent_link_keys.json")) as f:
         incon_keys = json.load(f, object_pairs_hook=OrderedDict)
-    with open(inval_keys) as f:
+    with open(test_data_path("miserables_invalid_link_keys.json")) as f:
         inval_keys = json.load(f, object_pairs_hook=OrderedDict)
 
     # Act: Test all the data files and save their outcomes

7087cd9e0358a3dc2057719c0349398f50155c06
JackWilb <jackwilburn@tutanota.com>
2019-11-21 13:54:32 -0700

    Move the workspace exists code and explicitly test all the workspaces (even those not created)

--- client/test/e2e.test.js
+++ client/test/e2e.test.js
@@ -17,6 +17,7 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+// Clicks the add workspace button, types a name, and clicks the button
 async function create_workspace(p, name) {
     await p.waitForSelector("#add-workspace");
     await p.click("#add-workspace")
@@ -26,6 +27,24 @@ async function create_workspace(p, name) {
     await p.click("#create-workspace")
 }
 
+async function workspace_exists(p, name) {
+    let exists = false;
+
+    await p.waitForSelector(".v-list-item__title");
+    let workspaces = await p.evaluate(() => {
+        let titles = []
+        let doc_nodes = document.querySelectorAll('.v-list-item__title'); 
+        for (node of doc_nodes) {
+            titles.push(node.innerText)
+        }
+        return titles});
+
+    exists = workspaces.includes(name)
+
+    return exists
+}
+
+// Start of tests
 test('e2e-client-test-valid-actions', async (t) => {
     // Arrange: Set up the page
     const b = await browser(width, height);
@@ -37,10 +56,8 @@ test('e2e-client-test-valid-actions', async (t) => {
     await create_workspace(p, 'puppeteer')
 
     // Assert: Check that the new workspace exists with no tables
-    await p.waitForSelector(".v-list-item");
-    await p.waitForSelector(".v-list-item__title");
-    const workspace_name = await p.evaluate(() => document.querySelector('.v-list-item__title').innerText);
-    t.equal(workspace_name, "puppeteer", "The new workspace is created and called the right thing.")
+    let exists = await workspace_exists(p, "puppeteer")
+    t.equal(exists, true, "The new workspace is created and called the right thing.")
 
     // Assert: Check that there are no tables or graphs yet
     await p.waitForSelector(".ws-detail-empty-list");
@@ -88,10 +105,14 @@ test('e2e-client-test-invalid-actions', async (t) => {
 
     // Assert: Check that the new workspace exists with no tables
     await sleep(200)
-    await p.waitForSelector(".v-list-item");
-    await p.waitForSelector(".v-list-item__title");
-    const workspace_name = await p.evaluate(() => document.querySelectorAll('.v-list-item__title')[0].innerText);
-    t.equal(workspace_name, "a", "Invalid workspaces weren't created but the last valid one was.")
+    let exists = await workspace_exists(p, "123")
+    t.equal(exists, false, "Workspace called '123' wasn't created.")
+
+    exists = await workspace_exists(p, "++--==__")
+    t.equal(exists, false, "Workspace called '++--==__' wasn't created.")
+
+    exists = await workspace_exists(p, "a")
+    t.equal(exists, true, "Workspace called 'a' was created.")
 
     // Act: Try to add broken versions of a node table, an edge table, and a graph
     await p.waitForSelector("#add-table");

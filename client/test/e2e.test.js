2711591d52bf322cc0558fa2a005971867a5d3fb
Roni Choudhury <roni.choudhury@kitware.com>
2019-11-22 17:11:52 -0500

    Fix lint issues with test file

--- client/test/e2e.test.js
+++ client/test/e2e.test.js
@@ -6,126 +6,126 @@ const height = 1080;
 
 // Opens the chromium window
 function browser(width, height) {
-    return puppeteer.launch({
+  return puppeteer.launch({
     headless: true,
     args: [`--window-size=${width},${height}`],
     // slowMo: 20 // For testing
-    });
+  });
 }
 
 function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 // Clicks the add workspace button, types a name, and clicks the button
 async function create_workspace(p, name) {
-    await p.waitForSelector('#add-workspace');
-    await p.click('#add-workspace')
-    await p.waitForSelector('#workspace-name');
-    await p.focus('#workspace-name')
-    await p.keyboard.type(name)
-    await p.click('#create-workspace')
+  await p.waitForSelector('#add-workspace');
+  await p.click('#add-workspace');
+  await p.waitForSelector('#workspace-name');
+  await p.focus('#workspace-name');
+  await p.keyboard.type(name);
+  await p.click('#create-workspace');
 }
 
 async function workspace_exists(p, name) {
-    let exists = false;
+  let exists = false;
 
-    await p.waitForSelector('.v-list-item__title');
-    let workspaces = await p.evaluate(() => {
-        let titles = []
-        let doc_nodes = document.querySelectorAll('.v-list-item__title'); 
-        for (node of doc_nodes) {
-            titles.push(node.innerText)
-        }
-        return titles
-    });
+  await p.waitForSelector('.v-list-item__title');
+  let workspaces = await p.evaluate(() => {
+    let titles = [];
+    let doc_nodes = document.querySelectorAll('.v-list-item__title');
+    for (let node of doc_nodes) {
+      titles.push(node.innerText);
+    }
+    return titles;
+  });
 
-    exists = workspaces.includes(name)
+  exists = workspaces.includes(name);
 
-    return exists
+  return exists;
 }
 
 async function element_exists(empty = false, element_type, p, name) {
-    let exists, list_is_empty, tables;
+  let exists, list_is_empty, tables;
 
-    await p.waitForSelector('[data-title="Tables"] .ws-detail-empty-list ');
-    tables = await p.evaluate(element_type => {
-        let titles = []
-        let doc_nodes = document.querySelectorAll('[data-title="' + element_type + '"] .ws-detail-empty-list '); 
-        for (node of doc_nodes) {
-            titles.push(node.innerText)
-        }
-        return titles
-    }, element_type);
-
-    if (empty) {
-        list_is_empty = tables.includes("info There's nothing here yet...")
-        return list_is_empty
-    } else {
-        exists = tables.includes(name)
-        return exists
+  await p.waitForSelector('[data-title="Tables"] .ws-detail-empty-list ');
+  tables = await p.evaluate(element_type => {
+    let titles = [];
+    let doc_nodes = document.querySelectorAll('[data-title="' + element_type + '"] .ws-detail-empty-list ');
+    for (let node of doc_nodes) {
+      titles.push(node.innerText);
     }
+    return titles;
+  }, element_type);
+
+  if (empty) {
+    list_is_empty = tables.includes('info There\'s nothing here yet...');
+    return list_is_empty;
+  } else {
+    exists = tables.includes(name);
+    return exists;
+  }
 }
 
 
 // Start of tests
 test('e2e - Check that actions that should work, do work', async (t) => {
-    // Arrange: Set up the page
-    const b = await browser(width, height);
-    const p = await b.newPage();
-    await p.setViewport({ width, height });
-    await p.goto('http://127.0.0.1:58080/');
-    
-    // Act: Test creating a workspace
-    await create_workspace(p, 'puppeteer')
+  // Arrange: Set up the page
+  const b = await browser(width, height);
+  const p = await b.newPage();
+  await p.setViewport({ width, height });
+  await p.goto('http://127.0.0.1:58080/');
 
-    // Assert: Check that the new workspace exists with no tables
-    let exists = await workspace_exists(p, 'puppeteer')
-    t.ok(exists, 'Workspace called "puppeteer" was created.')
+  // Act: Test creating a workspace
+  await create_workspace(p, 'puppeteer');
 
-    // Assert: Check that there are no tables or graphs yet
-    exists = await element_exists(true, "Tables", p, undefined)
+  // Assert: Check that the new workspace exists with no tables
+  let exists = await workspace_exists(p, 'puppeteer');
+  t.ok(exists, 'Workspace called "puppeteer" was created.');
 
-    t.ok(exists, 'The new workspace has no tables.')
+  // Assert: Check that there are no tables or graphs yet
+  exists = await element_exists(true, 'Tables', p, undefined);
 
-    exists = await element_exists(true, "Graphs", p, undefined)
-    t.ok(exists, 'The new workspace has no graphs.')
+  t.ok(exists, 'The new workspace has no tables.');
 
-    // Cleanup
-    await b.close();
-    t.end();
+  exists = await element_exists(true, 'Graphs', p, undefined);
+  t.ok(exists, 'The new workspace has no graphs.');
+
+  // Cleanup
+  await b.close();
+  t.end();
 });
 
 test('e2e - Check that actions that shouldn\'t work, don\'t work', async (t) => {
-    // Arrange: Set up the page
-    const b = await browser(width, height);
-    const p = await b.newPage();
-    await p.setViewport({ width, height });
-    await p.goto('http://127.0.0.1:58080/');
-    
-    // Act: Test creating invalid workspaces
-    await create_workspace(p, '123')
-    await p.click('#workspace-name', { clickCount: 3 })
-    await p.click('#add-workspace') // Close the modal (this will cause a failure in the next command if it is made)
+  // Arrange: Set up the page
+  const b = await browser(width, height);
+  const p = await b.newPage();
+  await p.setViewport({ width, height });
+  await p.goto('http://127.0.0.1:58080/');
 
-    await create_workspace(p, '++--==__')
-    await p.click('#workspace-name', { clickCount: 3 })
-    await p.click('#add-workspace') // Close the modal (this will cause a failure in the next command if it is made)
+  // Act: Test creating invalid workspaces
+  await create_workspace(p, '123');
+  await p.click('#workspace-name', { clickCount: 3 });
+  await p.click('#add-workspace'); // Close the modal (this will cause a failure in the next command if it is made)
 
-    await create_workspace(p, 'a')
+  await create_workspace(p, '++--==__');
+  await p.click('#workspace-name', { clickCount: 3 });
+  await p.click('#add-workspace'); // Close the modal (this will cause a failure in the next command if it is made)
 
-    // Assert: Check that the new workspace exists with no tables
-    await sleep(200)
-    let exists = await workspace_exists(p, '123')
-    t.notOk(exists, 'Workspace called "123" wasn\'t created.')
+  await create_workspace(p, 'a');
 
-    exists = await workspace_exists(p, '++--==__')
-    t.notOk(exists, 'Workspace called "++--==__" wasn\'t created.')
+  // Assert: Check that the new workspace exists with no tables
+  await sleep(200);
+  let exists = await workspace_exists(p, '123');
+  t.notOk(exists, 'Workspace called "123" wasn\'t created.');
 
-    exists = await workspace_exists(p, 'a')
-    t.ok(exists, 'Workspace called "a" was created.')
+  exists = await workspace_exists(p, '++--==__');
+  t.notOk(exists, 'Workspace called "++--==__" wasn\'t created.');
 
-    // Cleanup
-    await b.close();
-    t.end();
+  exists = await workspace_exists(p, 'a');
+  t.ok(exists, 'Workspace called "a" was created.');
+
+  // Cleanup
+  await b.close();
+  t.end();
 });

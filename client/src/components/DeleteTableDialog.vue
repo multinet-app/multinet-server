ef64887d06a0f3dbf683eb5f23384db2642b82aa
Roni Choudhury <roni.choudhury@kitware.com>
2019-12-27 15:34:12 -0500

    Disallow table deletion if an existing graph is using one or more of them

--- client/src/components/DeleteTableDialog.vue
+++ client/src/components/DeleteTableDialog.vue
@@ -24,7 +24,7 @@
       </v-tooltip>
     </template>
 
-    <v-card>
+    <v-card v-if="!dependentGraphs">
       <v-card-title
         class="headline pb-0 pt-3 px-5"
         primary-title
@@ -52,7 +52,38 @@
           @click="dialog = false"
           >cancel</v-btn>
       </v-card-actions>
+    </v-card>
+    <v-card v-else>
+      <v-card-title
+        class="headline pb-0 pt-3 px-5"
+        primary-title
+        >
+        Delete Tables
+      </v-card-title>
 
+      <v-card-text class="px-5 py-4">
+        The following graphs are using these tables:
+        <ul>
+          <li v-for="graph in using">
+            {{ graph }}
+          </li>
+        </ul>
+      </v-card-text>
+
+      <v-card-text class="px-5 py-4">
+        You must delete these graphs before you can delete the tables.
+      </v-card-text>
+
+      <v-divider />
+
+      <v-card-actions class="px-4 py-3">
+        <v-spacer />
+
+        <v-btn
+          depressed
+          @click="dialog = false"
+          >ok</v-btn>
+      </v-card-actions>
     </v-card>
 
   </v-dialog>
@@ -82,6 +113,7 @@ export default Vue.extend({
       dialog: false,
       disabled: true,
       timeout: undefined as number | undefined,
+      using: [] as string[],
     };
   },
 
@@ -94,15 +126,24 @@ export default Vue.extend({
     nonZeroSelection(): boolean {
       return this.selection.length > 0;
     },
+
+    dependentGraphs(): boolean {
+      return this.using.length > 0;
+    },
   },
 
   watch: {
-    dialog() {
+    async dialog() {
       if (this.dialog) {
-        this.timeout = window.setTimeout(() => {
-          this.disabled = false;
-          this.timeout = undefined;
-        }, 2000);
+        const using = await this.findDependentGraphs();
+        if (using.length > 0) {
+          this.using = using;
+        } else {
+          this.timeout = window.setTimeout(() => {
+            this.disabled = false;
+            this.timeout = undefined;
+          }, 2000);
+        }
       } else {
         window.clearTimeout(this.timeout);
         this.disabled = true;
@@ -124,6 +165,27 @@ export default Vue.extend({
 
       this.$emit('deleted');
       this.dialog = false;
+    },
+
+    async findDependentGraphs() {
+      const {
+        selection,
+        workspace,
+      } = this;
+
+      const graphNames = await api.graphs(workspace);
+
+      console.log(selection);
+
+      const using = [] as string[];
+      for (const graph of graphNames) {
+        const data = await api.graph(workspace, graph);
+        if (selection.some((table) => data.edgeTable === table || data.nodeTables.indexOf(table) > -1)) {
+          using.push(graph);
+        }
+      }
+
+      return using;
     },
   },
 });

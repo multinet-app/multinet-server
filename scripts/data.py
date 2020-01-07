5365bb0d82942a1b22694c836c8e7f8599108ee4
Jake Nesbitt <jjnesbitt2@gmail.com>
2020-01-07 12:08:29 -0500

    Add table creation in data script

--- scripts/data.py
+++ scripts/data.py
@@ -1,9 +1,16 @@
 """Script that populates initial data into the multinet backend."""
+
+import csv
 from pathlib import Path
-from typing import Tuple, List, Iterable
+from typing import Tuple, List
+
+from multinet.db import create_workspace, get_workspace_db
+from multinet.errors import WorkspaceNotFound
+
+WORKSPACE_NAME = "example_data"
 
 
-def determine_table_types(paths: Iterable[Path]) -> Tuple[Path]:
+def determine_table_types(paths) -> Tuple:
     """
     Given 2 csv file names, determines the edge table and node table.
 
@@ -62,8 +69,32 @@ if __name__ == "__main__":
     data_dir = Path(__file__).absolute().parents[1] / "data"
 
     for path in data_dir.iterdir():
-        files = tuple(path.glob("*.csv"))
+        dataset_name = path.name
+        print(f'Processing dataset "{dataset_name}"')
 
+        files = tuple(path.glob("*.csv"))
         node_table, edge_table = determine_table_types(files)
 
-        # Upload node and edge table nwo
+        try:
+            workspace = get_workspace_db(WORKSPACE_NAME)
+        except WorkspaceNotFound:
+            create_workspace(WORKSPACE_NAME)
+            workspace = get_workspace_db(WORKSPACE_NAME)
+
+        for i, file in enumerate((node_table, edge_table)):
+            table_name = file.stem
+            edge = i == 1
+
+            with file.open(mode="r") as csv_file:
+                rows = list(csv.DictReader(csv_file))
+
+            if workspace.has_collection(table_name):
+                print(f'\tTable "{table_name}" already exists, skipping...')
+            else:
+                coll = workspace.create_collection(table_name, edge=edge)
+                inserted = len(coll.insert_many(rows))
+                table_type = "edge" if edge else "node"
+
+                print(
+                    f'\tInserted {inserted} rows into {table_type} table "{table_name}"'
+                )

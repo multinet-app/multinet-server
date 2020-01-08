57c982bc7ec81a3cc85092c17a4bd4b3ddc3dd5c
Jacob Nesbitt <jjnesbitt2@gmail.com>
2020-01-08 12:43:23 -0500

    (Linting) Fix code style

--- client/src/views/WorkspaceDetail.vue
+++ client/src/views/WorkspaceDetail.vue
@@ -192,16 +192,14 @@ export default Vue.extend({
       try {
         nodeTables = await api.tables(this.workspace, { type: 'node' });
         edgeTables = await api.tables(this.workspace, { type: 'edge' });
-      }
-      catch(err) {
-        if (err.status === 404 && err.statusText === "Workspace Not Found") {
-          this.$router.push({name: "home"})
-        }
-        else {
-          throw err
+      } catch (err) {
+        if (err.status === 404 && err.statusText === 'Workspace Not Found') {
+          this.$router.push({name: 'home'});
+        } else {
+          throw err;
         }
 
-        return
+        return;
       }
 
       this.tables = nodeTables.concat(edgeTables);

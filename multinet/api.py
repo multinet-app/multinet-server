0edc222b17b509aa4f2e9defc1b47c474014955f
Jacob Nesbitt <jjnesbitt2@gmail.com>
2020-01-16 10:45:07 -0500

    Add comments to explain data

--- multinet/api.py
+++ multinet/api.py
@@ -150,13 +150,15 @@ def create_graph(workspace: str, graph: str, edge_table: Optional[str] = None) -
     if loaded_workspace.has_graph(graph):
         raise AlreadyExists("Graph", graph)
 
+    # Get reference tables with respective referenced keys,
+    # tables in the _from column and tables in the _to column
     edge_table_properties = util.get_edge_table_properties(workspace, edge_table)
-    node_tables = edge_table_properties["table_keys"]
+    referenced_tables = edge_table_properties["table_keys"]
     from_tables = edge_table_properties["from_tables"]
     to_tables = edge_table_properties["to_tables"]
 
     errors = []
-    for table, keys in node_tables.items():
+    for table, keys in referenced_tables.items():
         if not loaded_workspace.has_collection(table):
             errors.append(f"Reference to undefined table: {table}")
         else:
--- multinet/types.py
+++ multinet/types.py
@@ -10,6 +10,11 @@ TableType = Literal["all", "node", "edge"]
 class EdgeTableProperties(TypedDict):
     """Describes gathered information about an edge table."""
 
+    # Dictionary mapping all referenced tables to the referenced keys within that table
     table_keys: Dict[str, Set[str]]
+
+    # Keeps track of which tables are referenced in the _from column
     from_tables: Set[str]
+
+    # Keeps track of which tables are referenced in the _to column
     to_tables: Set[str]

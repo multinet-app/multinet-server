35792205bcd282b48a41182d13a16afb47789159
Roni Choudhury <roni.choudhury@kitware.com>
2020-02-04 15:49:28 -0500

    Refactor file-reading utility into a function

--- client/vue.config.js
+++ client/vue.config.js
@@ -6,22 +6,25 @@ const webpack = require('webpack');
 
 const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin');
 
+function fileToJsonString(filename) {
+  let result = null;
+  if (fs.existsSync(filename)) {
+    result = JSON.stringify(fs.readFileSync(filename).toString().trim());
+  }
+
+  return result;
+}
+
 // Read in .env file.
 const env = dotenv.parse(fs.readFileSync(path.resolve('..', '.env')));
 process.env.FLASK_SERVE_PORT = process.env.FLASK_SERVE_PORT || env.FLASK_SERVE_PORT || 5000;
 
 // Look for a git-sha file; if found, inject the value found in it into the
 // application.
-let GIT_SHA = null;
-if (fs.existsSync('git-sha')) {
-  GIT_SHA = JSON.stringify(fs.readFileSync('git-sha').toString().trim());
-}
+const GIT_SHA = fileToJsonString('git-sha');
 
 // Inject a value for gaTag (google analytics) if present as well.
-let GA_TAG = null;
-if (fs.existsSync('ga-tag')) {
-  GA_TAG = JSON.stringify(fs.readFileSync('ga-tag').toString().trim());
-}
+const GA_TAG = fileToJsonString('ga-tag');
 
 module.exports = {
   configureWebpack: {

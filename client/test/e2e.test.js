4907fec95c99aaacf6f09f507cd94b11e1556df4
Roni Choudhury <roni.choudhury@kitware.com>
2020-02-11 13:05:22 -0500

    Update the "allowed" test into a more comprehensive version
    The new test checks for a workspace name it can use, then creates that
    workspace, examines it, and finally deletes it, all the while making
    testing assertions as it goes. The net effect on the database is zero,
    making this suitable for use with a live dev client/server.

--- client/test/e2e.test.js
+++ client/test/e2e.test.js
@@ -28,16 +28,34 @@ function browser(width, height) {
 
 // Sets up the browser with some default settings
 async function setup() {
+  // Create a browser.
   const b = await browser(width, height);
+
+  // Create a page.
   const p = await b.newPage();
   await p.setViewport({ width, height });
-  await p.goto(url);
   await p.setDefaultTimeout(5000);
+
+  // Navigate to the app, and dismiss the "got it" dialog.
+  await p.goto(url);
+  await p.waitForSelector('#got-it');
+  await p.click('#got-it');
+
   return [b, p];
 }
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function get_element_coords(p, selector) {
+  const el = await p.$(selector);
+  const bb = await el.boundingBox();
+
+  return {
+    x: bb.x + bb.width / 2,
+    y: bb.y + bb.height / 2,
+  };
 }
 
 // Clicks the add workspace button, types a name, and clicks the button
@@ -50,12 +68,29 @@ async function create_workspace(p, name) {
   await p.click('#create-workspace');
 }
 
-// Checks that a workspace exists in the left pane
-async function workspace_exists(p, name) {
-  let exists = false;
+async function delete_workspace(p, name) {
+  // Move the mouse over the workspace entry to make the checkbox appear.
+  const entry = `a[href="#/workspaces/${name}/"]`;
+  const coords = await get_element_coords(p, entry);
+  await p.mouse.move(coords.x, coords.y);
 
+  // Click on the checkbox, then on the delete icon.
+  const selector = `${entry} input`;
+  await p.waitForSelector(selector);
+  await p.click(selector);
+  await p.click('#delete-workspaces');
+
+  // Wait for the delete dialog to appear, and for the "Yes" button to become
+  // active.
+  await sleep(4000);
+
+  // Click on the "yes".
+  await p.click('#delete-workspace-yes');
+}
+
+async function get_workspace_names(p) {
   await p.waitForSelector('.v-list-item__title');
-  let workspaces = await p.evaluate(() => {
+  return p.evaluate(() => {
     let titles = [];
     let doc_nodes = document.querySelectorAll('.v-list-item__title');
     for (let node of doc_nodes) {
@@ -63,9 +98,14 @@ async function workspace_exists(p, name) {
     }
     return titles;
   });
+}
 
-  exists = workspaces.includes(name);
-  return exists;
+// Checks that a workspace exists in the left pane
+async function workspace_exists(p, name) {
+  await p.waitForSelector('.v-list-item__title');
+  const workspaces = await get_workspace_names(p);
+
+  return workspaces.includes(name);
 }
 
 // Get the names of either all tables or all graphs in the current workspace
@@ -106,9 +146,57 @@ async function elements_empty(element_type, p) {
   return list_is_empty;
 }
 
+// Declare global variables for the browser and page objects.
+let b;
+let p;
+
+test('Create a valid workspace', async (t) => {
+  // Set up the browser/page.
+  [b, p] = await setup();
+
+  // First, figure out a name we can use for the workspace.
+  const workspaces = await get_workspace_names(p);
+  let name;
+  const limit = 1000;
+  let i;
+  for (i = 0; i < limit; i++) {
+    name = `puppeteer${i}`;
+    if (!workspaces.includes(name)) {
+      break;
+    }
+  }
+  if (i === limit) {
+    throw new Error('fatal: could not find an unused name');
+  }
+
+  // Create the workspace.
+  await create_workspace(p, name);
+  await sleep(500);
+
+  // Check that the new workspace now exists.
+  const exists = await workspace_exists(p, name);
+  t.ok(exists, `Workspace "${name}" was created`);
+
+  // Check that the new workspace has no tables or networks.
+  const tables = await elements_empty('Tables', p);
+  t.ok(tables, 'The new workspace has no tables');
+
+  const networks = await elements_empty('Networks', p);
+  t.ok(networks, 'The new workspace has no networks');
+
+  // Delete the workspace.
+  await delete_workspace(p, name);
+  await sleep(1000);
+  const deleted = !await workspace_exists(p, name);
+  t.ok(deleted, `Workspace "${name}" was deleted`);
+
+  await b.close();
+
+  t.end();
+});
 
 // Start of tests
-test('e2e - Check that actions that should work, do work', async (t) => {
+test.skip('e2e - Check that actions that should work, do work', async (t) => {
   // Arrange: Set up the page
   const [b, p] = await setup();
 
@@ -132,7 +220,7 @@ test('e2e - Check that actions that should work, do work', async (t) => {
   t.end();
 });
 
-test('e2e - Check that actions that shouldn\'t work, don\'t work', async (t) => {
+test.skip('e2e - Check that actions that shouldn\'t work, don\'t work', async (t) => {
   // Arrange: Set up the page
   const [b, p] = await setup();
 

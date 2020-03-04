2f28dba091cfdc1b7344387ca6ac65b0502ff3f9
Roni Choudhury <roni.choudhury@kitware.com>
2020-03-04 14:33:27 -0500

    Remove client and multinetjs directories
    See
    - https://github.com/multinet-app/multinet-client
    - https://github.com/multinet-app/multinetjs
    
    for replacement repos.

--- client/src/environment.ts
+++
@@ -1,3 +0,0 @@
-export const host: string = process.env.VUE_APP_MULTINET_HOST || 'http://localhost:5000';
-export const gaTag: string = process.env.VUE_APP_GA_TAG || '';
-export const gitSha: string = process.env.VUE_APP_GIT_SHA || '';

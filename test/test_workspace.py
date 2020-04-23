e038b7cb565a60ffdf075c08cca5d718f33a97f5
Jacob Nesbitt <jjnesbitt2@gmail.com>
2020-04-23 19:20:05 -0400

    Refactor tests using pytest fixtures

--- test/test_workspace.py
+++ test/test_workspace.py
@@ -1,4 +1,6 @@
 """Test that workspace operations act like we expect them to."""
+import pytest
+
 from uuid import uuid4
 from multinet.db import (
     create_workspace,
@@ -8,33 +10,47 @@ from multinet.db import (
 )
 
 
-def test_workspace_create_and_delete():
-    """Tests that creating a workspace doesn't result in invalid caching."""
+@pytest.fixture
+def workspace():
+    """
+    Create a workspace, and yield the name of the workspace.
+
+    On teardown, deletes the workspace.
+    """
+
     workspace_name = uuid4().hex
-
     create_workspace(workspace_name)
-    exists_after_create = workspace_exists(workspace_name)
-
-    # Teardown
+    yield workspace_name
     delete_workspace(workspace_name)
-    exists_after_delete = workspace_exists(workspace_name)
-
-    assert exists_after_create
-    assert not exists_after_delete
 
 
-def test_workspace_rename():
+def test_workspace_create(workspace):
+    """Test that creating a workspace doesn't result in invalid caching."""
+    assert workspace_exists(workspace)
+
+
+def test_workspace_delete(workspace):
+    """Tests that deleting a workspace doesn't result in invalid caching."""
+
+    delete_workspace(workspace)
+    exists = workspace_exists(workspace)
+
+    # Restore workspace, so the fixture can clean up
+    create_workspace(workspace)
+
+    assert not exists
+
+
+def test_workspace_rename(workspace):
     """Test that renaming a workspace doesn't result in invalid caching."""
-    workspace_name_a = uuid4().hex
-    workspace_name_b = uuid4().hex
-    create_workspace(workspace_name_a)
-    rename_workspace(workspace_name_a, workspace_name_b)
+    new_workspace_name = uuid4().hex
+    rename_workspace(workspace, new_workspace_name)
 
-    new_exists = workspace_exists(workspace_name_b)
-    old_exists = workspace_exists(workspace_name_a)
+    new_exists = workspace_exists(new_workspace_name)
+    old_exists = workspace_exists(workspace)
 
-    # Teardown
-    delete_workspace(workspace_name_b)
+    # Restore workspace, so the fixture can clean up
+    rename_workspace(new_workspace_name, workspace)
 
     assert new_exists
     assert not old_exists

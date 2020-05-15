f71ce1686f446763d176a6c0cecdccb5eca01c72
Roni Choudhury <roni.choudhury@kitware.com>
2020-05-15 16:35:52 -0400

    Switch "cors_hosts" to "allowed_origins"

--- multinet/__init__.py
+++ multinet/__init__.py
@@ -21,21 +21,21 @@ sentry_dsn = os.getenv("SENTRY_DSN", default="")
 sentry_sdk.init(dsn=sentry_dsn, integrations=[FlaskIntegration()])
 
 
-def get_cors_hosts() -> List[str]:
+def get_allowed_origins() -> List[str]:
     """Read in comma-separated CORS hosts list from environment."""
-    cors_hosts = os.getenv("CORS_HOSTS", default=None)
-    if cors_hosts is None:
+    allowed_origins = os.getenv("ALLOWED_ORIGINS", default=None)
+    if allowed_origins is None:
         return []
 
-    return cors_hosts.split(",")
+    return allowed_origins.split(",")
 
 
 def create_app(config: Optional[MutableMapping] = None) -> Flask:
     """Create a Multinet app instance."""
     app = Flask(__name__)
 
-    cors_hosts = get_cors_hosts()
-    CORS(app, origins=cors_hosts, supports_credentials=True)
+    allowed_origins = get_allowed_origins()
+    CORS(app, origins=allowed_origins, supports_credentials=True)
     Swagger(app, template_file="swagger/template.yaml")
 
     app.secret_key = flask_secret_key()

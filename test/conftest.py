60b6f0695fafd0474fbe7dd58802d56a87ac07c1
Jacob Nesbitt <jjnesbitt2@gmail.com>
2020-06-23 17:46:14 -0400

    Add data_directory and populated_workspace fixtures

--- test/conftest.py
+++ test/conftest.py
@@ -2,9 +2,12 @@
 
 import pytest
 from uuid import uuid4
+from pathlib import Path
 
 from multinet import create_app
 from multinet.db import create_workspace, delete_workspace
+
+from typing import Tuple, List
 
 
 @pytest.fixture
@@ -19,6 +22,14 @@ def server(app):
     """Return a test client to `app`."""
     client = app.test_client()
     return client
+
+
+@pytest.fixture
+def data_directory() -> Path:
+    """Return the path to the test data directory."""
+
+    # return os.path.abspath(os.path.join(os.path.dirname(__file__), "data"))
+    return Path(__file__).parent / "data"
 
 
 @pytest.fixture
@@ -41,3 +52,26 @@ def handled_workspace(generated_workspace):
     yield generated_workspace
 
     delete_workspace(generated_workspace)
+
+
+@pytest.fixture
+def populated_workspace(
+    handled_workspace, data_directory, server
+) -> Tuple[str, Tuple[List[str], List[str]]]:
+    """
+    Populate a workspace with some data.
+
+    Returns a Nested Tuple of the form:
+    `(workspace_name: str, (graphs: List[str], tables: List[str]))`
+
+    """
+    with open(Path(data_directory) / "miserables.json") as miserables:
+        data = miserables.read()
+
+    resp = server.post(f"/api/d3_json/{handled_workspace}/miserables", data=data)
+    assert resp.status_code == 200
+
+    return (
+        handled_workspace,
+        (["miserables"], ["miserables_nodes", "miserables_links"]),
+    )
--- test/test_meta.py
+++ test/test_meta.py
@@ -5,3 +5,25 @@ def test_generated_workspace(handled_workspace, server):
     """Test that a generated workspace exists when querying the API."""
     resp = server.get(f"/api/workspaces/{handled_workspace}")
     assert resp.status_code == 200
+
+
+def test_populated_workspace(populated_workspace, server):
+    """Test that the populated workspace has a graph, an edge and node table."""
+    workspace, (graphs, tables) = populated_workspace
+
+    # Graphs
+    resp = server.get(f"/api/workspaces/{workspace}/graphs")
+    assert resp.status_code == 200
+
+    graphs = resp.json
+    assert len(graphs) == 1
+    assert graphs[0] == "miserables"
+
+    # Tables
+    resp = server.get(f"/api/workspaces/{workspace}/tables")
+    assert resp.status_code == 200
+
+    tables = resp.json
+    assert len(tables) == 2
+    assert "miserables_nodes" in tables
+    assert "miserables_links" in tables

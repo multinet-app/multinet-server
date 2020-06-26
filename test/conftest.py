a59c53ad0cde95c9dc643cba5594f77fef51c8f3
Jacob Nesbitt <jjnesbitt2@gmail.com>
2020-06-26 18:25:13 -0400

    Update create_workspace call to include user
    - Add handled_user pytest fixture
    - Update test in test_meta.py to set a session cookie

--- test/conftest.py
+++ test/conftest.py
@@ -2,9 +2,11 @@
 
 import pytest
 from uuid import uuid4
+from dataclasses import asdict
 
 from multinet import create_app
 from multinet.db import create_workspace, delete_workspace
+from multinet.user import register_user, set_user_cookie, user_collection, UserInfo
 
 
 @pytest.fixture
@@ -22,12 +24,38 @@ def server(app):
 
 
 @pytest.fixture
-def generated_workspace():
+def handled_user():
+    """
+    Create a user, and yield that user.
+
+    On teardown, deletes the user.
+    """
+    user = set_user_cookie(
+        register_user(
+            UserInfo(
+                family_name="test",
+                given_name="test",
+                name="test test",
+                picture="",
+                email="test@test.test",
+                sub=uuid4().hex,
+            )
+        )
+    )
+
+    yield user
+
+    # TODO: Once the function exists, use `delete_user` here instead
+    user_collection().delete(asdict(user))
+
+
+@pytest.fixture
+def generated_workspace(handled_user):
     """Create a workspace, and yield the name of the workspace."""
 
     workspace_name = uuid4().hex
 
-    create_workspace(workspace_name)
+    create_workspace(workspace_name, handled_user)
     return workspace_name
 
 

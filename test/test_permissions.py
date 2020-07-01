2c5eab4d9dd6ec73389d7b19239c5b3795c5600f
Roni Choudhury <roni.choudhury@kitware.com>
2020-07-01 10:40:40 -0400

    Add a test for `require_reader`

---
+++ test/test_permissions.py
@@ -0,0 +1,10 @@
+from multinet.user import MULTINET_COOKIE
+
+
+def test_require_reader(server, handled_workspace, handled_user):
+    with server.session_transaction() as session:
+        session[MULTINET_COOKIE] = handled_user.multinet.session
+
+    resp = server.get(f"/api/workspaces/{handled_workspace}")
+    assert resp.status_code == 200
+    assert resp.json['permissions']['owner'] == handled_user.sub

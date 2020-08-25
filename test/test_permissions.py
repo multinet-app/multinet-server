3cfddea4376efe3049a6a1e89e689769203981db
Jacob Nesbitt <jjnesbitt2@gmail.com>
2020-08-25 14:49:23 -0400

    Update tests to use ORM classes

--- test/test_permissions.py
+++ test/test_permissions.py
@@ -6,7 +6,7 @@ import conftest
 def test_require_reader(server, managed_workspace, managed_user):
     """Test the `require_reader` decorator."""
     with conftest.login(managed_user, server):
-        resp = server.get(f"/api/workspaces/{managed_workspace}/permissions")
+        resp = server.get(f"/api/workspaces/{managed_workspace.name}/permissions")
 
     assert resp.status_code == 200
     assert resp.json["owner"]["sub"] == managed_user.sub

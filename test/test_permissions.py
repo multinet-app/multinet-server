ca2c4c5b64e66b1989e85ab8ed743e42aafc2a42
Jacob Nesbitt <jjnesbitt2@gmail.com>
2020-10-13 17:10:52 -0400

    Add test for user logout

--- test/test_permissions.py
+++ test/test_permissions.py
@@ -10,3 +10,15 @@ def test_require_reader(server, managed_workspace, managed_user):
 
     assert resp.status_code == 200
     assert resp.json["owner"]["sub"] == managed_user.sub
+
+
+# Corresponding login test is in test_meta
+def test_logout(server, managed_workspace, managed_user):
+    """Test that logout works properly."""
+
+    with conftest.login(managed_user, server):
+        resp = server.get(f"/api/user/logout")
+        assert resp.status_code == 200
+
+        resp = server.get(f"/api/workspaces/{managed_workspace.name}/permissions")
+        assert resp.status_code == 401

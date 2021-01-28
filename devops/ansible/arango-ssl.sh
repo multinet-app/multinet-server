5fedc41dd7aa1ed52f056c31aab838c00a39b8fd
JackWilb <jackwilburn@tutanota.com>
2021-01-27 17:18:32 -0700

    Update ssl script to output to the home directory, use SERVER_NAME

--- devops/ansible/arango-ssl.sh
+++ devops/ansible/arango-ssl.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 
-cat /etc/letsencrypt/live/multinet.app/fullchain.pem /etc/letsencrypt/live/multinet.app/privkey.pem > /etc/letsencrypt/live/multinet.app/server.pem
-cp /etc/letsencrypt/live/multinet.app/server.pem /home/ubuntu/server.pem
+cat /etc/letsencrypt/live/SERVER_NAME/fullchain.pem /etc/letsencrypt/live/SERVER_NAME/privkey.pem > /home/ubuntu/server.pem
 chmod 644 /home/ubuntu/server.pem
 
 systemctl restart arangodb3
